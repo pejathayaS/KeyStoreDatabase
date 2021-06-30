a9a7b9eb48126d4bea3c4f422ab6d92e81e71c4f
pejathayaS <40621568+pejathayaS@users.noreply.github.com>
2021-06-30 12:44:35 +0530

    Delete com.interview directory

--- com.interview/src/main/java/interview/botstrapengine/Constants.java
+++
@@ -1,9 +0,0 @@
-package interview.botstrapengine;
-
-public final class Constants {
-
-  public static final String readOnlyFilePath = "C:\\Performance\\readOnlyFile.txt";
-  public static final String keyValueStorageMaster = "C:\\Performance\\keyValueStorage.txt";
-  public static final String tempFileName = "C:\\Performance\\temp.txt";
-
-}
